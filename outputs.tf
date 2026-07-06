bbffee4d04135f207325c5eed834c32ffb555b4d
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:38 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "management_locks" {
   description = "All management_lock resources"
   value       = azurerm_management_lock.management_locks
 }
+output "management_locks_lock_level" {
+  description = "List of lock_level values across all management_locks"
+  value       = [for k, v in azurerm_management_lock.management_locks : v.lock_level]
+}
+output "management_locks_name" {
+  description = "List of name values across all management_locks"
+  value       = [for k, v in azurerm_management_lock.management_locks : v.name]
+}
+output "management_locks_notes" {
+  description = "List of notes values across all management_locks"
+  value       = [for k, v in azurerm_management_lock.management_locks : v.notes]
+}
+output "management_locks_scope" {
+  description = "List of scope values across all management_locks"
+  value       = [for k, v in azurerm_management_lock.management_locks : v.scope]
+}
 
