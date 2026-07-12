6687154b7e96303e00147960945ba638c85d59e0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "management_locks_id" {
+  description = "Map of id values across all management_locks, keyed the same as var.management_locks"
+  value       = { for k, v in azurerm_management_lock.management_locks : k => v.id }
+}
 output "management_locks_lock_level" {
   description = "Map of lock_level values across all management_locks, keyed the same as var.management_locks"
   value       = { for k, v in azurerm_management_lock.management_locks : k => v.lock_level }
